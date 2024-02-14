2a20fa246eae3ce7a762aa551e68e5a9547df53f
abcc24 <dirge20@gmail.com>
2024-02-14 18:56:02 +0100

    dd

---
+++ secondo.py
@@ -0,0 +1,55 @@
+pippo=10
+paperino="30"
+ziopaperone="TRUE"
+
+"""
+
+Definire
+- due variabili (i1, i2) di tipo intero
+- due variabili (s1, s2) di tipo stringa
+- due variabili (f1, f2) di tipo float
+- due variabili (b1, b2) di tipo boolean
+stampare
+i1+i2
+s1+s2
+f1+f2
+b1+b2
+i1+s1
+s1+i2
+i1+f2
+f2+i2
+i1+b1
+f1+b1
+s1+b1
+b1+i1
+b1+s1
+b1+f1
+"""
+
+i1 = 10
+i2 = 20
+s1 = "batman"
+s2 = "spiderman"
+f1 = 20.5
+f2 = 30.5
+b1 = True
+b2 = False
+
+print (i1+i2) # definisco un'addizione di due valori numerici
+print (s1+s2) # definisco un'addizione di due stringhe
+print (f1+f2) # definisco un'addizione di due valori float
+print (b1+b2) # definisco un'addizione di due valori booleani
+#print (i1+s1) # definisco un'addizione impossibile di un valore numerico e una stringa
+#print (s1+i2) # definisco un'addizione impossibile di una stringa e valore numerico
+print (i1+f2) # definisco un'addizione di valore numerico e un valore float
+print (f2+i2) # definisco un'addizione di un valore float e un numero intero
+print (i1+b1) # definisco un'addizione di un valore numerico e un booleano (True equivale a 1)
+print (f1+b1) # definisco un'addizione di un float e un booleano
+#print (s1+b1) # definisco un'addizione impossibile 
+print (b1+i1) # definisco un'addizione
+#print (b1+s1) # definisco un'addizione impossibile
+print (b1+f1) # definisco un'addizione
+
+
+
+

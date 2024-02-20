ba7443028b69768d1d98bf4f1e61f239ae802539
abcc24 <dirge20@gmail.com>
2024-02-20 13:26:56 +0100

    dd

--- es per fare.py
+++ es per fare.py
@@ -2,6 +2,6 @@ carattere=input("inserire carattere ").lower()
 vocali="aeiou"
 
 if carattere in vocali:
-    print (f"il carattere" '{carattere}' "è una vocale")
+    print ("il carattere", carattere, "è una vocale")
 else:
-    print (f"il carattere" '{carattere}' "non è una vocale")
+    print ("il carattere", carattere, "non è una vocale")

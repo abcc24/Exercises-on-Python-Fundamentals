9a37ef424752725f8d4997da6138fcd9d6821fd1
abcc24 <dirge20@gmail.com>
2024-02-20 13:16:38 +0100

    dd

---
+++ es per fare.py
@@ -0,0 +1,7 @@
+carattere=input("inserire carattere ").lower()
+vocali="aeiou"
+
+if carattere in vocali:
+    print (f"il carattere '{carattere}' è una vocale")
+else:
+    print (f"il carattere '{carattere}' non è una vocale")

801de6187b355e66acb0710ec959eb87ee455fbc
abcc24 <dirge20@gmail.com>
2024-02-21 15:51:45 +0100

    ddd

---
+++ calcolatrice.py
@@ -0,0 +1,14 @@
+
+a=float(input("inserisci numero " ))
+action=(input("inserisci operazione " ))
+b=float(input("inserisci nuemro " ))
+
+
+if action=="+":
+    print (a+b)
+elif action=="-":
+    print (a-b)
+elif action=="*":
+    print (a*b)
+elif action=="/":
+    print (a/b)

9f869cfeedb95e1a8737c6339bc6e69a382d5d7a
abcc24 <dirge20@gmail.com>
2024-02-22 15:53:23 +0100

    eeee

---
+++ terzo es.py
@@ -0,0 +1,15 @@
+a = print
+a("Ciao come va?")
+print=100
+a("ok")
+a=20
+
+#all'inizio assegnamo ad "a" la funzione di print
+#non posso più stampare perché "a" perde il valore di "print" alla riga 5
+
+
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0 
+
+#secondo a quale casting si assegna (str,int,float) il valore di 3 cambia
